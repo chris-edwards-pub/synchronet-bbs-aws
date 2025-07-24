afb3e2e79f1b1c9a2fef36e3b6e93e2b4edff901
Chris Edwards <chris@edwards.pub>
2025-07-24 14:44:30 -0500

    initial commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -75,6 +75,36 @@ resource "aws_security_group" "sbbs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # MQTT TLS port for secure connections
+  ingress {
+    from_port   = 8883
+    to_port     = 8883
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # MQTT WebSocket TLS port for secure web interface
+  ingress {
+    from_port   = 8884
+    to_port     = 8884
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
