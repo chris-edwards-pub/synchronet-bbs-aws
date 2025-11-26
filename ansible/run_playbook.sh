4bc9cf503448031eb995b8662986751a4ebe9abe
Chris Edwards <6855954+chris-edwards-pub@users.noreply.github.com>
2025-11-26 15:44:00 -0600

    changing dns records

--- ansible/run_playbook.sh
+++ ansible/run_playbook.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Pass all command-line arguments to ansible-playbook
+# Usage examples:
+#   ./run_playbook.sh --tags websocket
+#   ./run_playbook.sh --skip-tags ssl
+#   ./run_playbook.sh --limit synchronet
+#   ./run_playbook.sh --check --diff
+#   ./run_playbook.sh --tags websocket --verbose
+
 echo "Downloading inventory from S3..."
 aws s3 cp s3://synchronet-bbs/ansible-sbbs/hosts.ini ./hosts.ini
 
 if [ $? -eq 0 ]; then
-    echo "Running Ansible playbook..."
-    ansible-playbook -i hosts.ini install_synchronet.yaml
+    echo "Running Ansible playbook with arguments: $*"
+    ansible-playbook -i hosts.ini install_synchronet.yaml "$@"
 else
     echo "Failed to download inventory from S3"
     exit 1
